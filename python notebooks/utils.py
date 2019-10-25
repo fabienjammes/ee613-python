f17c16aa541b5da5a0d62551257ccab4b090927b
teguhSL <tarsle14@gmail.com>
2019-10-25 18:00:20 +0200

    - refining Linear Regression Example

--- python notebooks/utils.py
+++ python notebooks/utils.py
@@ -62,6 +62,7 @@ def plot_gaussian_2D(mu, sigma,ax,color=[0.7,0.7,0.7],alpha=1.0, label='label'):
 def plot_data_2D(x, y_true, y_pred, colors = ['r', 'k', 'b'], title = 'Linear regression', alphas = None):
     if alphas is None:
         alphas = np.ones(len(x))
+        
     #plot the predicted data
     plt.plot(x,y_pred, '-' + colors[0])
     
@@ -70,8 +71,8 @@ def plot_data_2D(x, y_true, y_pred, colors = ['r', 'k', 'b'], title = 'Linear re
         #plot the true data
         plt.plot(x[i],y_true[i], '.' + colors[1])#,alpha = alphas[i])
         plt.plot([x[i],x[i]], [y_true[i], y_pred[i]], '-'+colors[2], alpha = alphas[i])
-    plt.xlabel(u'x\u2081')
-    plt.ylabel(u'y\u2081')
+    plt.xlabel('x')
+    plt.ylabel('y')
     
     plt.title(title)
     
